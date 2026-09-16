e82490ae0e1a9508fff077bbc7310331d50b47eb
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-16 13:21:04 +0330

    feat(web): add organization users page

---
+++ apps/web/src/app/organization/users/page.tsx
@@ -0,0 +1,5 @@
+import { OrganizationUsersOverview } from "@/components/organization/OrganizationUsersOverview";
+
+export default function OrganizationUsersPage() {
+  return <OrganizationUsersOverview />;
+}
