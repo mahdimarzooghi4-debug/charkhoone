585ecdf4441f75f264392cad8b3ee747fe084699
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-17 12:49:18 +0330

    feat(observability): wire Worker OpenTelemetry

--- src/Charkhoone.Worker/Program.cs
+++ src/Charkhoone.Worker/Program.cs
@@ -1,9 +1,11 @@
 using Charkhoone.Infrastructure;
+using Charkhoone.Infrastructure.Observability;
 using Charkhoone.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCharkhooneObservability(builder.Configuration, "Charkhoone.Worker");
 
 var rabbitMqOptions = RabbitMqWorkerOptions.FromConfiguration(builder.Configuration);
 builder.Services.AddSingleton(rabbitMqOptions);
