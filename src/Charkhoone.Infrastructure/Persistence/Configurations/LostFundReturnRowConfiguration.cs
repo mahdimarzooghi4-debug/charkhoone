2218e1781b893163cdca33faf1f6889c3e21e9f4
mahdi marzooghi <mahdimarzooghi4@gmail.com>
2026-09-17 10:44:16 +0330

    feat(persistence): map lost fund return tracking

---
+++ src/Charkhoone.Infrastructure/Persistence/Configurations/LostFundReturnRowConfiguration.cs
@@ -0,0 +1,30 @@
+using Charkhoone.Infrastructure.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Charkhoone.Infrastructure.Persistence.Configurations;
+
+public sealed class LostFundReturnRowConfiguration : IEntityTypeConfiguration<LostFundReturnRow>
+{
+    public void Configure(EntityTypeBuilder<LostFundReturnRow> builder)
+    {
+        builder.ToTable("lost_fund_returns");
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.CoveragePaymentId).IsUnique();
+        builder.HasIndex(x => new { x.ContractId, x.WithdrawnAtUtc });
+
+        builder.Property(x => x.WithdrawnAmountRial).HasPrecision(38, 18).IsRequired();
+        builder.Property(x => x.MonthlyRate).HasPrecision(10, 8).IsRequired();
+        builder.Property(x => x.CalculationPolicyVersion).HasMaxLength(64);
+        builder.Property(x => x.CalculatedReturnRial).HasPrecision(38, 18);
+
+        builder.HasOne<LeaseContractRow>()
+            .WithMany()
+            .HasForeignKey(x => x.ContractId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne<CoveragePaymentRow>()
+            .WithOne()
+            .HasForeignKey<LostFundReturnRow>(x => x.CoveragePaymentId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
